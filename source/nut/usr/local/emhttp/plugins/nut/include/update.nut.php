e619b10ff39b3547650b7889c4ef24a0e5db8d52
dmacias72 <derekmacias@live.com>
2017-04-29 09:09:04 -0600

    move rc script

--- source/nut/usr/local/emhttp/plugins/nut/include/update.nut.php
+++ source/nut/usr/local/emhttp/plugins/nut/include/update.nut.php
@@ -17,7 +17,7 @@
 $cfg  = "/boot/config/plugins/nut/nut.cfg";
 $connection = $_POST['DRIVER']=='custom' ? $_POST['SERIAL'] : $_POST['DRIVER'];
 
-exec("/etc/rc.d/rc.nut stop");
+exec('/etc/rc.d/rc.nut stop');
 exec("sed -i -e '/^SERVICE/c\\SERVICE=\"'{$_POST['SERVICE']}'\"' $cfg");
 exec("sed -i -e '/^PORT/c\\PORT=\"'{$_POST['PORT']}'\"' $cfg");
 exec("sed -i -e '/^MODE/c\\MODE=\"'{$_POST['MODE']}'\"' $cfg");
@@ -28,5 +28,5 @@ exec("sed -i -e '/^TIMEOUT/c\\TIMEOUT=\"'{$_POST['TIMEOUT']}'\"' $cfg");
 exec("sed -i -e '/^UPSKILL/c\\UPSKILL=\"'{$_POST['UPSKILL']}'\"' $cfg");
 exec("sed -i -e '/^DRIVER/c\\DRIVER=\"'{$connection}'\"' $cfg");
 
-if ($_POST['SERVICE']=='enable') exec("/usr/local/emhttp/plugins/nut/scripts/rc.nut start");
+if ($_POST['SERVICE']=='enable') exec('/etc/rc.d/rc.nut start');
 ?>
