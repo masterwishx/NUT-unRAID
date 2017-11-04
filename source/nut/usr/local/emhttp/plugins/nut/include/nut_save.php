aa9f3c980490615eaea1e8dba96e3878f234ebaa
dmacias72 <derekmacias@live.com>
2017-11-03 18:23:37 -0600

    add save to plugin path

--- source/nut/usr/local/emhttp/plugins/nut/include/nut_save.php
+++ source/nut/usr/local/emhttp/plugins/nut/include/nut_save.php
@@ -1,14 +1,27 @@
 <?
-$base = '/boot/config/plugins/nut/ups/';
+$base     = '/etc/nut/';
+$plgpath  = '/boot/config/plugins/nut/ups/';
 $editfile = realpath($_POST['editfile']);
+$plgfile  = $plgpath.basename($editfile);
 
 if(!strpos($editfile, $base) && file_exists($editfile) && array_key_exists('editdata', $_POST)){
     // remove carriage returns
     $editdata = str_replace("\r", '', $_POST['editdata']);
 
-    // get previous config file contents and save them
-    $editdata_old = (file_exists($editfile)) ? file_get_contents($editfile) : '';
-    file_put_contents($editfile.'.old', $editdata_old);
+    if($_POST['manual'] == 'enable'){
+        if(! is_dir($plgpath))
+            mkdir($plgpath);
+
+        // get previous config file contents and save them
+        if(file_exists($plgfile)){
+            $plgfile_old = file_get_contents($plgfile);
+            file_put_contents($plgfile.'.old', $plgfile_old);
+        }
+
+        file_put_contents($plgfile, $editdata);
+    }
+
+    file_put_contents($editfile, $editdata);
 
     // save file contents
     $return_var = file_put_contents($editfile, $editdata);
