1d0ccd47afa9c1419d25b041ea6a20832f1fe09c
gfjardim <gfjardim@gmail.com>
2020-03-12 10:13:49 -0300

    Deal with ups.alarms
    Since I don't know if upsc client can export multiple ups.alarm attributes, I added support for multiple instances of it.
    
    An animation was added too.

--- source/nut/usr/local/emhttp/plugins/nut/include/nut_footer.php
+++ source/nut/usr/local/emhttp/plugins/nut/include/nut_footer.php
@@ -21,12 +21,17 @@ $all    = $_GET['all']=='true';
 function get_ups($name, $ip="localhost")
 {
   $output = [];
+  $alarm = 0;
   if (file_exists('/var/run/nut/upsmon.pid')) {
     // echo "/bin/upsc ".escapeshellarg($name)."@".escapeshellarg($ip)." 2>/dev/null";
     exec("/usr/bin/upsc ".escapeshellarg($name)."@".escapeshellarg($ip)." 2>/dev/null", $rows);
     for ($i=0; $i<count($rows); $i++) {
       $row = array_map('trim', explode(':', $rows[$i], 2));
-      $output[$row[0]] = $row[1];
+      $prop = $row[0];
+      if (stripos($prop, "ups.alarm")) {
+        $prop = "${prop}".$alarm++;
+      }
+      $output[$prop] = $row[1];
     }
   }
   return $output;
@@ -38,8 +43,12 @@ function array_key_exists_wildcard ( $arr, $nee )
     return array_values(preg_grep( '/^' . $nee . '$/i', array_keys( $arr ) ));
 }
 
-$ups_status = get_ups($nut_name, $nut_ip);
 $status = [];
+$ups_status = get_ups($nut_name, $nut_ip);
+// $ups_status["ups.alarm1"] = "alarm1";
+// $ups_status["ups.alarm2"] = "alarm2";
+// $ups_status["ups.alarm3"] = "alarm3";
+// $ups_status["ups.alarm4"] = "alarm4";
 if (count($ups_status)) {
   $online  = ( array_key_exists("ups.status", $ups_status) && stripos($ups_status["ups.status"],'OL')!==false );
   $battery = (array_key_exists("battery.charge",$ups_status)) ? intval(strtok($ups_status['battery.charge'],' ')) : false;
@@ -53,21 +62,30 @@ if (count($ups_status)) {
     $power   = intval($nut_powerw);
   }
 
+  $ups_alarm = array_key_exists_wildcard($ups_status, 'ups.alarm*');
+  if (count($ups_alarm)) {
+    $alarms = "";
+    foreach ($ups_alarm as $al) {
+      $alarms .= "<div><i class='fa fa-exclamation-circle orange-text'></i>&nbsp;".$ups_status[$al]."</div>";
+    }
+    $status[3] = "<span id='nut_alarm' $orange data=\"$alarms\"><i class='fa fa-bell faa-ring animated'></i>&thinsp;</span>";
+  }
+
   if ($battery !== false) {
-    if ($online && $battery < 100) $icon = "<span id='nut_battery' $green title='${nut_name}: online - battery is charging'><i class='fa fa-battery-charging'></i>&thinsp;${battery}%</span>";
-    else if ($online && $battery  == 100) $icon = "<span id='nut_battery' $green title='${nut_name}: online - battery is full'><i class='fa fa-battery-full'></i>&thinsp;${battery}%</span>";
-    else if (!$online) $icon = "<span id='nut_battery' $red title='${nut_name}: offline - battery is discharging'><i class='fa fa-battery-discharging'></i>&thinsp;${battery}%</span>";
-    else $icon = "<span id='nut_battery' $green title='${nut_name}: battery status unknown'><i class='fa fa-battery-discharging'></i>n/a</span>";
+    if ($online && $battery < 100) $icon = "<span id='nut_battery' $green data='${nut_name}: online - battery is charging'><i class='fa fa-battery-charging'></i>&thinsp;${battery}%</span>";
+    else if ($online && $battery  == 100) $icon = "<span id='nut_battery' $green data='${nut_name}: online - battery is full'><i class='fa fa-battery-full'></i>&thinsp;${battery}%</span>";
+    else if (!$online) $icon = "<span id='nut_battery' $red data='${nut_name}: offline - battery is discharging'><i class='fa fa-battery-discharging'></i>&thinsp;${battery}%</span>";
+    else $icon = "<span id='nut_battery' $green data='${nut_name}: battery status unknown'><i class='fa fa-battery-discharging'></i>n/a</span>";
 
     $status[0] = $icon;
   } else {
-    $status[0] = "<span id='nut_battery' style='margin:0 6px 0 12px' title='$nut_name: battery info not available'><i class='fa fa-battery-empty'></i>&thinsp;n/a</span>";
+    $status[0] = "<span id='nut_battery' style='margin:0 6px 0 12px' data='$nut_name: battery info not available'><i class='fa fa-battery-empty'></i>&thinsp;n/a</span>";
   }
   $wattage = round($power*$load*0.01)."w";
-  if ($power && $load) $status[1] = "<span id='nut_power' title='${nut_name}: consuming $wattage ($load% of capacity)' ".($load>=90 ? "$red" : "$green")."><i class='fa fa-plug'></i>&thinsp;$wattage</span>";
+  if ($power && $load) $status[1] = "<span id='nut_power' data='${nut_name}: consuming $wattage ($load% of capacity)' ".($load>=90 ? "$red" : "$green")."><i class='fa fa-plug'></i>&thinsp;$wattage</span>";
 
   echo "<span>".implode('</span><span style="margin:0 6px 0 12px">', $status)."</span>";
 } else {
-  echo "<span style='margin:0 6px 0 12px' id='nut_power' class='tooltip-nut' title='$nut_name: UPS info not availabe, check your settings'><i class='fa fa-battery-empty'></i>&thinsp;n/a</span>";
+  echo "<span style='margin:0 6px 0 12px' id='nut_power' class='tooltip-nut' data='$nut_name: UPS info not availabe, check your settings'><i class='fa fa-battery-empty'></i>&thinsp;n/a</span>";
 }
 ?>
